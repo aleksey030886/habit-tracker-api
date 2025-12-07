ca037241aea406a1700984e250cf9065593105c2
Алексей <mac@MacBook-Air-Aleksej.local>
2025-12-07 17:29:12 +0600

    api postgrer

---
+++ src/models/Habit.ts
@@ -0,0 +1,60 @@
+import sequelize from "../config/database";
+import { DataTypes, Model } from "sequelize";
+
+class Habit extends Model {
+
+}
+
+Habit.init (
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    goal: {
+        type: DataTypes.INTEGER,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Habit',
+    tableName: 'habits',
+    timestamps: true,
+  },
+)
+
+console.log(Habit === sequelize.models.Habit); 
+
+export default Habit;
+
+// const Habit = sequelize.define(
+//   'Habit',
+  // {
+  //   id: {
+  //       type: DataTypes.INTEGER,
+  //       primaryKey: true,
+  //       autoIncrement: true,
+  //   },
+  //   title: {
+  //     type: DataTypes.STRING,
+  //     allowNull: false,
+  //   },
+  //   description: {
+  //     type: DataTypes.STRING,
+  //   },
+  //   goal: {
+  //       type: DataTypes.INTEGER,
+  //   },
+  // },
+  // {
+    
+//   },
+// );
